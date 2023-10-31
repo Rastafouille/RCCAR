9d136a8a3a2fcd213c0e50f6bef06641f23f67fd
rastafouille <jseyssaud@yahoo.fr>
2023-10-31 21:46:04 +0100

    maj

--- launch/jetson.bash
+++ launch/jetson.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-motion
-echo "motion started"
-
 sudo chmod +77 /dev/ttyACM0
 echo "chmod /dev/ttyACM0 done"
+
+motion -m
+echo "motion started"
 
 exit 0
---
+++ launch/pc.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+firefox 192.168.1.97:8080
+exit 0
