b919f97caec46bd12cdc8dac5f7eb6f4af9c7f98
rastafouille <jseyssaud@yahoo.fr>
2023-11-03 14:55:45 +0100

    maj

--- script/tf_odom.py
+++ script/tf_odom.py
@@ -9,11 +9,11 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
 def OdomCallback(data):
-    odom_quat = tf.transformations.quaternion_from_euler(0, 0, data.pose.pose.orientation.z)
+    odom_quat = tf.transformations.quaternion_from_euler(0, 0, -data.pose.pose.orientation.z)
     odom_broadcaster.sendTransform((data.pose.pose.position.x, data.pose.pose.position.y, 0.),odom_quat,data.header.stamp,"base_link","odom")
 
-rospy.init_node('odometry_publisher')
-odom_pub = rospy.Publisher("odom", Odometry, queue_size=1)
+rospy.init_node('odometry_publisher_node')
+#odom_pub = rospy.Publisher("odom", Odometry, queue_size=1)
 rospy.Subscriber('/odom', Odometry,OdomCallback,queue_size=1)
 odom_broadcaster = tf.TransformBroadcaster()
 
--- script/vesc_to_odom.py
+++ script/vesc_to_odom.py
@@ -68,81 +68,95 @@ from std_msgs.msg import Float64
 #   float64 distance_traveled
 #   int32 fault_code
 
+class vesc_to_odom:
+	def __init__(self):
+		rospy.init_node('vesc_to_odom_node2')
+		self.gear_ratio=86/27
+		self.wheel_diameter=0.088
+		self.angle_ratio =55*2*math.pi/360
+		self.nbre_pole_moteur=4
+		self.wheelbase=0.3
+		self.speed_to_erpm_gain=rospy.get_param("/speed_to_erpm_gain")
+		self.speed_to_erpm_offset=rospy.get_param("/speed_to_erpm_offset")
+		self.steering_angle_to_servo_gain=rospy.get_param("/steering_angle_to_servo_gain")
+		self.steering_angle_to_servo_offset=rospy.get_param("/steering_angle_to_servo_offset")
+
+		self.last_servo_position=0
+		self.x=0
+		self.y=0
+		self.theta=0
+		self.odom=Odometry()
+		self.odom.header.frame_id='odom'
+		self.odom.child_frame_id='base_link'
+
+		self.last_state=VescStateStamped()
+		self.last_time=rospy.Time.now()
+
+		self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=1)
+		rospy.Subscriber('/sensors/core', VescStateStamped,self.StateCallback,queue_size=1)
+		rospy.Subscriber('/sensors/servo_position_command', Float64,self.ServoCallback,queue_size=1)
 
 
-rospy.init_node('vesc_to_odom_node2')
-gear_ratio=86/27
-wheel_diameter=0.088
-angle_ratio = math.pi/4
-nbre_pole_moteur=4
-wheelbase=0.3
-speed_to_erpm_gain=rospy.get_param("/speed_to_erpm_gain")
-speed_to_erpm_offset=rospy.get_param("/speed_to_erpm_offset")
-steering_angle_to_servo_gain=rospy.get_param("/steering_angle_to_servo_gain")
-steering_angle_to_servo_offset=rospy.get_param("/steering_angle_to_servo_offset")
 
-def erpm_to_speed(erpm):
-	speed=erpm/nbre_pole_moteur*2*(2*pi*wheel_diameter/2)
-	return speed 
+	def erpm_to_speed(self,erpm):
+		speed=erpm/self.nbre_pole_moteur*2*(2*math.pi*self.wheel_diameter/2)/self.gear_ratio/60
+		return speed 
 
-def servo_to_angle(servo):
-	angle=(servo-steering_angle_to_servo_offset)/steering_angle_to_servo_gain*angle_ratio
-	return angle 
-
-last_servo_position=0
-x=0
-y=0
-theta=0
-odom=Odometry()
-odom.header.frame_id='odom'
-odom.child_frame_id='base_link'
-
-last_state=VescStateStamped()
-global last_time
-last_time=rospy.Time.now()
-
-#listener = tf.TransformListener()
+	def servo_to_angle(self,servo):
+		angle=(servo-self.steering_angle_to_servo_offset)/self.steering_angle_to_servo_gain*self.angle_ratio
+		print('angle=',angle)
+		return angle 
 
 
-def StateCallback(data):
-	speed=data.state.speed-speed_to_erpm_offset
-	dt=data.header.stamp-last_time
-	current_speed = erpm_to_speed(data.state.speed - speed_to_erpm_offset)
-	current_angular_velocity = current_speed * math.tan(servo_to_angle(last_servo_position)) / wheelbase
-	yaw= current_angular_velocity*dt
-	x_dot = current_speed*math.cos(yaw)
-	y_dot = current_speed *sin(yaw)
-	x=x_dot*dt
-	y=y_dot*dt
-	odom.pose.pose.position.x += x
-	odom.pose.pose.position.y += y
-	odom.pose.pose.orientation.x = 0.0
-	odom.pose.pose.orientation.y = 0.0
-	odom.pose.pose.orientation.z = math.sin(yaw / 2.0)
-	odom.pose.pose.orientation.w = math.cos(yaw / 2.0)
-	#tolerance
-	odom.pose.covariance[0]=0.2  #x
-	odom.pose.covariance[7]=0.2  #y
-	odom.pose.covariance[35]=0.4  #yaw
-	#Vitesse
-	odom.twist.twist.linear.x = current_speed
-	odom.twist.twist.linear.y = 0.0
-	odom.twist.twist.angular.z = current_angular_velocity
+	def StateCallback(self,data):
+		
+		dt=data.header.stamp-self.last_time
+		#print ('dt=', dt.to_sec())
+		current_speed = self.erpm_to_speed(-data.state.speed)# - self.speed_to_erpm_offset)
+		print('current_speed=',current_speed)
+		current_angular_velocity = current_speed * math.tan(self.servo_to_angle(self.last_servo_position)) / self.wheelbase
+		print('current_angular_velocity=',current_angular_velocity)
+		yaw= current_angular_velocity*dt.to_sec()
+		print('yaw=',yaw)
+		x_dot = current_speed*math.cos(yaw)
+		y_dot = current_speed *math.sin(yaw)
+		x=x_dot*dt.to_sec()
+		y=y_dot*dt.to_sec()
+		self.odom.pose.pose.position.x += x
+		self.odom.pose.pose.position.y += y
+		self.odom.pose.pose.orientation.x = 0.0
+		self.odom.pose.pose.orientation.y = 0.0
+		self.odom.pose.pose.orientation.z += math.sin(yaw)# / 2.0)
+		self.odom.pose.pose.orientation.w += math.cos(yaw)# / 2.0)
+			#tolerance
+		self.odom.pose.covariance[0]=0.2  #x
+		self.odom.pose.covariance[7]=0.2  #y
+		self.odom.pose.covariance[35]=0.4  #yaw
+			#Vitesse
+		self.odom.twist.twist.linear.x = current_speed
+		self.odom.twist.twist.linear.y = 0.0
+		self.odom.twist.twist.angular.z = current_angular_velocity
 
-	odom.header.stamp=data.header.stamp
-	odom_pub.publish(odom)
-	last_time= data.header.stamp
+		self.odom.header.stamp=data.header.stamp
+		self.odom_pub.publish(self.odom)
+		self.last_time= data.header.stamp
 
-def ServoCallback(data):
-	last_servo_position=data.data
+	def ServoCallback(self,data):
+		self.last_servo_position=data.data
 
-
-odom_pub = rospy.Publisher("odom", Odometry, queue_size=1)
-rospy.Subscriber('/sensors/core', VescStateStamped,StateCallback,queue_size=1)
-rospy.Subscriber('/sensors/servo_position_command', Float64,ServoCallback,queue_size=1)
+		
 
 
-r = rospy.Rate(30.0)
-while not rospy.is_shutdown():
-        r.sleep()
+	
+
+
+if __name__ == '__main__':
+    try:
+        vesc_to_odom=vesc_to_odom() #our main function
+    except rospy.ROSInterruptException:
+        print ('interrupted !')
+    rate = rospy.Rate(30) #hz
+
+    while not rospy.is_shutdown():
+        rate.sleep()
 
