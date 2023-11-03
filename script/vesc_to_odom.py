ed99445514fe16d88f725b39408f0459abb4823b
rastafouille <jseyssaud@yahoo.fr>
2023-11-03 16:17:52 +0100

    maj

--- script/vesc_to_odom.py
+++ script/vesc_to_odom.py
@@ -72,10 +72,10 @@ class vesc_to_odom:
 	def __init__(self):
 		rospy.init_node('vesc_to_odom_node2')
 		self.gear_ratio=64/26
-		self.wheel_diameter=0.088
-		self.angle_ratio =30*2*math.pi/360
+		self.wheel_diameter=0.080
+		self.angle_ratio =40*2*math.pi/360
 		self.nbre_pole_moteur=4
-		self.wheelbase=0.3
+		self.wheelbase=0.27
 		self.speed_to_erpm_gain=rospy.get_param("/speed_to_erpm_gain")
 		self.speed_to_erpm_offset=rospy.get_param("/speed_to_erpm_offset")
 		self.steering_angle_to_servo_gain=rospy.get_param("/steering_angle_to_servo_gain")
@@ -95,7 +95,8 @@ class vesc_to_odom:
 		self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=1)
 		rospy.Subscriber('/sensors/core', VescStateStamped,self.StateCallback,queue_size=1)
 		rospy.Subscriber('/sensors/servo_position_command', Float64,self.ServoCallback,queue_size=1)
-
+		
+		self.odom_broadcaster = tf.TransformBroadcaster()
 
 
 	def erpm_to_speed(self,erpm):
@@ -110,44 +111,56 @@ class vesc_to_odom:
 
 	def StateCallback(self,data):
 		
+		
+		current_time = rospy.Time.now()
 		dt=data.header.stamp-self.last_time
 		#print ('dt=', dt.to_sec())
 		current_speed = self.erpm_to_speed(-data.state.speed)# - self.speed_to_erpm_offset)
 		print('current_speed=',current_speed)
-		current_angular_velocity = current_speed * math.tan(self.servo_to_angle(self.last_servo_position)) / self.wheelbase
+		current_angular_velocity = current_speed * math.tan(self.servo_to_angle(self.last_servo_position))# / self.wheelbase
 		print('current_angular_velocity=',current_angular_velocity)
 		self.theta+= current_angular_velocity*dt.to_sec()
 		print('theta=',self.theta)
 		x_dot = current_speed*math.cos(self.theta)
 		y_dot = current_speed *math.sin(self.theta)
-		x=x_dot*dt.to_sec()
-		y=y_dot*dt.to_sec()
-		self.odom.pose.pose.position.x += x
-		self.odom.pose.pose.position.y += y
-		self.odom.pose.pose.orientation.x = 0.0
-		self.odom.pose.pose.orientation.y = 0.0
-		self.odom.pose.pose.orientation.z = math.sin(self.theta / 2.0)
-		self.odom.pose.pose.orientation.w = math.cos(self.theta/ 2.0)
-			#tolerance
-		self.odom.pose.covariance[0]=0.2  #x
-		self.odom.pose.covariance[7]=0.2  #y
-		self.odom.pose.covariance[35]=0.4  #theta
-			#Vitesse
-		self.odom.twist.twist.linear.x = current_speed
-		self.odom.twist.twist.linear.y = 0.0
-		self.odom.twist.twist.angular.z = current_angular_velocity
+		self.x+=x_dot*dt.to_sec()
+		self.y+=y_dot*dt.to_sec()
 
-		self.odom.header.stamp=data.header.stamp
+
+
+
+
+		# self.odom.pose.pose.position.x += x
+		# self.odom.pose.pose.position.y += y
+		# self.odom.pose.pose.orientation.x = 0.0
+		# self.odom.pose.pose.orientation.y = 0.0
+		# self.odom.pose.pose.orientation.z = math.sin(self.theta / 2.0)
+		# self.odom.pose.pose.orientation.w = math.cos(self.theta/ 2.0)
+		# 	#tolerance
+		# self.odom.pose.covariance[0]=0.2  #x
+		# self.odom.pose.covariance[7]=0.2  #y
+		# self.odom.pose.covariance[35]=0.4  #theta
+		# 	#Vitesse
+		# self.odom.twist.twist.linear.x = current_speed
+		# self.odom.twist.twist.linear.y = 0.0
+		# self.odom.twist.twist.angular.z = current_angular_velocity
+
+		odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.theta)
+		self.odom_broadcaster.sendTransform((self.odom.pose.pose.position.x, self.odom.pose.pose.position.y, 0.),odom_quat,current_time,"base_link","odom")
+
+
+		self.odom.header.stamp=current_time
+		self.odom.pose.pose = Pose(Point(self.x, self.y, 0.), Quaternion(*odom_quat))
+		self.odom.twist.twist = Twist(Vector3(x_dot, y_dot, 0), Vector3(0, 0, current_angular_velocity))
 		self.odom_pub.publish(self.odom)
+
+		
 		self.last_time= data.header.stamp
 
 	def ServoCallback(self,data):
 		self.last_servo_position=data.data
 
 		
-
-
-	
 
 
 if __name__ == '__main__':
