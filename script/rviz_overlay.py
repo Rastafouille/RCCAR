4f34f71592126e59b0bc534220eb301feac6d590
rastafouille <jseyssaud@yahoo.fr>
2023-11-04 23:37:16 +0100

    maj

--- script/rviz_overlay.py
+++ script/rviz_overlay.py
@@ -5,9 +5,13 @@
 import rospy
 from std_msgs.msg import Float32, Float64, String
 
+from vesc_msgs.msg import VescStateStamped
+
 speed_percent=Float32()
 speed=String()
 steer=Float32()
+
+joy_max_speed=rospy.get_param("/joy_to_ackermann/joy_max_speed")
 speed_to_erpm_gain=rospy.get_param("/speed_to_erpm_gain")
 speed_to_erpm_offset=rospy.get_param("/speed_to_erpm_offset")
 steering_angle_to_servo_gain=rospy.get_param("/steering_angle_to_servo_gain")
@@ -15,14 +19,14 @@ steering_angle_to_servo_offset=rospy.get_param("/steering_angle_to_servo_offset"
 
 
 def SteerCallback(data):
-	steer.data=(data.data-steering_angle_to_servo_offset)*(-100)
+	steer.data=(data.data-steering_angle_to_servo_offset)/0.3*(-100)
 	steer_pub.publish(steer)
 
 def SpeedCallback(data):
-	if data.data>=0:
-		speed_percent.data=(data.data-speed_to_erpm_offset)/speed_to_erpm_gain*100
-	else:
-		speed_percent.data=-(data.data+speed_to_erpm_offset)/speed_to_erpm_gain*100
+	# if data.data>=0:
+	# 	speed_percent.data=(data.data-speed_to_erpm_offset)/(speed_to_erpm_gain*joy_max_speed)*100
+	# else:
+	speed_percent.data=data.state.speed/(speed_to_erpm_gain*joy_max_speed)*100
 	
 	speed_percent_pub.publish(speed_percent)
 	 
@@ -37,7 +41,7 @@ speed_pub = rospy.Publisher("rviz_speed", String, queue_size=1)
 steer_pub = rospy.Publisher("rviz_steer", Float32, queue_size=1)
 
 rospy.Subscriber('/commands/servo/position', Float64,SteerCallback,queue_size=1)
-rospy.Subscriber('/commands/motor/speed', Float64,SpeedCallback,queue_size=1)
+rospy.Subscriber('/sensors/core', VescStateStamped,SpeedCallback,queue_size=1)
 
 
 
